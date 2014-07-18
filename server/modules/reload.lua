90f1e59e8298fe96fc2ff3a5c224252d8d32e409
Seppi <seppi@josefnpat.com>
2014-07-18 16:18:41 -0400

    Reload now shows what modules are available

--- server/modules/reload.lua
+++ server/modules/reload.lua
@@ -6,7 +6,11 @@ end
 
 function reload:onHook(irc,user,channel,message,argv)
   reload_modules()
-  return "Modules reloaded."
+  local mod_names = {}
+  for i,v in pairs(global_modules) do
+    table.insert(mod_names,i)
+  end
+  return "Modules reloaded ("..table.concat(mod_names," ")..")"
 end
 
 return reload
