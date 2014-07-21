e67ce6dcdbca8de09deaa529a5b2c62a39766ecd
josefnpat <seppi@josefnpat.com>
2014-07-21 00:01:42 -0400

    This should fix the number error

--- server/modules/dice.lua
+++ server/modules/dice.lua
@@ -5,8 +5,12 @@ function module:getTrigger()
 end
 
 function module:onHook(irc,user,channel,message,argv)
-  local sides = tonumber(argv[2]) and tonumber(argv[2]) or 6
-  return "You roll a "..math.random(1,sides)
+  local sides = tonumber(argv[2])
+  if sides == nil or sides < 1 then
+    return "Usage: "..self:getTrigger().." [positive number]"
+  else
+    return "You roll a "..math.random(1,sides)
+  end
 end
 
 return module
