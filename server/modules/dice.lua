25bee7ed4903a687f5724ae8f2a2a55004053802
josefnpat <seppi@josefnpat.com>
2014-07-21 01:12:49 -0400

    Make dice max 9999 to avoid huuuuge number issues

--- server/modules/dice.lua
+++ server/modules/dice.lua
@@ -6,8 +6,8 @@ end
 
 function module:onHook(irc,user,channel,message,argv)
   local sides = tonumber(argv[2])
-  if sides == nil or sides < 1 then
-    return "Usage: "..self:getTrigger().." [positive number]"
+  if sides == nil or sides < 1 or sides > 9999 then
+    return "Usage: "..self:getTrigger().." [1..9999]"
   else
     return "You roll a "..math.random(1,sides)
   end
